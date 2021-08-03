3b114c39f6e0793fc4f1f7f53b62ce334e0c8d70
Mark Dawson <mark.dawson@polecat.com>
2021-08-03 09:32:06 +0100

    add package header

--- clubhouse-jump.el
+++ clubhouse-jump.el
@@ -1,3 +1,41 @@
+;;; clubhouse-jump.el --- Manage and navigate projects in Emacs easily -*- lexical-binding: t -*-
+
+;; Copyright © 2021 Mark Dawson <markgdawson@gmail.com>
+
+;; Author: Mark Dawson <bozhidar@batsov.com>
+;; URL: https://github.com/markgdawson/clubhouse-jump
+;; Keywords: project, convenience
+;; Version: 0.0.1-snapshot
+;; Package-Requires: ((emacs "25.1") org-clubhouse)
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; This library provides easy project management and navigation.  The
+;; concept of a project is pretty basic - just a folder containing
+;; special file.  Currently git, mercurial and bazaar repos are
+;; considered projects by default.  If you want to mark a folder
+;; manually as a project just create an empty .projectile file in
+;; it.  See the README for more details.
+;;
+;;; Code:
+
 ;; -----------------------------------------
 ;; Copied Utility Functions
 ;; -----------------------------------------
